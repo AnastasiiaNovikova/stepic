cb57eba1f64afaedbb4c69c980432fb06a3e9762
Mikhail Plekhanov <m.plekhanov@netris.ru>
2016-01-12 18:36:41 +0300

    Added solution for assignment 2 max sum.

---
+++ Algorithms/2_max_sum.py
@@ -0,0 +1,30 @@
+n = int(input())
+a = list(map(int, input().split(" ")))
+b = list(map(int, input().split(" ")))
+
+max_a = a[0]
+for i in range(1, len(b)):
+    if max_a > a[i]:
+        a[i] = max_a
+    elif a[i] > max_a:
+        max_a = a[i]
+
+max_b = b[len(b)-1]
+for i in reversed(range(0, len(b)-1)):
+    if max_b > b[i]:
+        b[i] = max_b
+    elif b[i] > max_b:
+        max_b = b[i]
+
+max_index_a = 0
+max_sum = a[0]+b[0]
+for i in range(0, len(b)):
+    if a[i] + b[i] > max_sum:
+        max_sum = a[i] + b[i]
+        max_index_a = i
+
+max_index_b = max_index_a
+while max_index_b + 1 < len(b) and b[max_index_b+1] == b[max_index_a]:
+    max_index_b += 1
+
+print(max_index_a, max_index_b)
