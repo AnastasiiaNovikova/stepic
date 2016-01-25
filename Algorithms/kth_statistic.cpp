9af72fd9a43a281d894c50fe0a6c3a605691540b
Mikhail Plekhanov <m.plekhanov@netris.ru>
2016-01-26 00:11:48 +0300

    Algorithms: Fixed order statistic algorithm.

--- Algorithms/kth_statistic.cpp
+++ Algorithms/kth_statistic.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <iterator>
 #include <algorithm>
-#include <array>
+#include <vector>
 
 template<class RandomIt>
 RandomIt debug(RandomIt first, RandomIt last) {
@@ -38,17 +38,16 @@ RandomIt partition(RandomIt first, RandomIt last) {
     }
     
     RandomIt pivot = median_pivot(first, last);
-    std::swap(*(last-1), *pivot);
     
-    RandomIt i = first;
-    RandomIt j = i;
-    while ( j < last-1 ) {
-        while ( *j > *pivot ) ++j;
-        if (j != (last-1))
-            std::swap( *(i++), *(j++) );
+    std::iter_swap(first, pivot);
+    
+    RandomIt i = first + 1;
+    for(RandomIt j = first + 1; j != last; ++j) {
+        if( *j < *first )
+            std::iter_swap(i++,j);
     }
-    while ( i < (last-2) && *i <= *pivot ) ++i;
-    std::swap( *i, *(last-1) );
+    std::iter_swap( --i, first );
+
     return i;
 }
 
@@ -56,37 +55,20 @@ template<class RandomIt>
 typename RandomIt::value_type kth_statistic(RandomIt first, RandomIt last, long k) {
     auto part = partition(first, last);
     
-    debug(first, last);
-    std::cout << "part = " << (part - first) << ", k = " << k << std::endl;
-    
     if (part - first == k) return *part;
     if (k < (part - first)) return kth_statistic(first, part, k);
-    else return kth_statistic(part+1, last, k - (part-first));
-}
-
-template<class RandomIt>
-void quickSort(RandomIt first, RandomIt last) {
-	auto part = partition(first, last);
-    /*
-	if (part > first) quickSort(first, part);
-	if (part != last) quickSort(std::next(part), last);
-	*/
-    if (part > first) quickSort(first, part);
-	if (part != last) quickSort(std::next(part), last);
+    else return kth_statistic(part+1, last, k - (part-first) - 1);
 }
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     
     long n, k;
-    std::cin >> n >> k;    
-    std::vector<long> a(std::istream_iterator<long>(std::cin), std::istream_iterator<long>());
+    std::cin >> n >> k;
+    std::vector<long> a(n);
+    a.assign(std::istream_iterator<long>(std::cin), std::istream_iterator<long>());
     
-    //std::cout << kth_statistic(a.begin(), a.end(), k ) << std::endl;
-    quickSort(a.begin(), a.end());
-	
-	std::copy(a.begin(), a.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << std::endl;
+    std::cout << kth_statistic(a.begin(), a.end(), k ) << std::endl;
     
     return 0;
 }
