1de635c4ce6b77a3430ff9ea5b4e84301c7efc43
Mikhail Plekhanov <m.plekhanov@netris.ru>
2016-01-25 14:56:00 +0300

    Algorithms: Optimized Quick Sort assignment.
    Tail recursion in QuickSort + Insertion Sort on arrays of 16 elements.

--- Algorithms/quicksort_assign.cpp
+++ Algorithms/quicksort_assign.cpp
@@ -3,81 +3,101 @@
 #include <algorithm>
 #include <vector>
 
+
 template<class RandomIt>
 void insertion_sort(RandomIt first, RandomIt last) {
-    RandomIt current = first+1;
-    while(current < last) {
+    for(RandomIt current = std::next(first); current < last; ++current) {
         auto temp = *current;
-        RandomIt it = current-1;
+        auto it = std::prev(current);
         for(; it >= first && temp < *it; --it)
             *(it+1) = *it;
         *(it+1) = temp;
-        current++;
     }
+}
+
+
+template< typename RandomIt, typename Comp >
+void shell_sort(RandomIt first, RandomIt last, Comp comp) {
+    static unsigned int gaps[] = { 1750, 701, 301, 132, 57, 23, 10, 4, 1 };
+    
+    for( auto d : gaps ) {
+        if ( d>= (last - first) )
+            continue;
+        for( RandomIt i = first + d; i != last; ++i)
+            for( RandomIt j = i; j - first >=d && comp( *j, *(j-d)); j-=d )
+                std::swap( *j, *(j-d) );
+    }
+}
+
+template< typename RandomIt >
+void shell_sort(RandomIt first, RandomIt last) {
+    shell_sort(first, last, std::less<typename RandomIt::value_type>());
 }
 
 template<class RandomIt>
 RandomIt partition(RandomIt first, RandomIt last) {
-	if (std::distance(first,last) <= 1) {
-		return first;
-	}
-	
-	auto pivot = std::prev(last);
-	auto mid = first;
-	std::advance(mid, std::distance(first,last)/2);
-	
-	if (*pivot > *first && *pivot > *mid) {
-		if (*first > *mid)
-			pivot = first;
-		else
-			pivot = mid;
-	}
-	else if (*pivot < *first && *pivot < *mid) {
-		if (*first < *mid)
-			pivot = first;
-		else
-			pivot = mid;
-	}
-	
-	std::swap(*first, *pivot);
-	
-	auto i=std::next(first), j = std::prev(last);
-	while ( i <= j ) {
-		while(i < last && *i <= *first) ++i;
-		while(*j > *first) --j;
-		if( i < j )
-			std::swap( *(i++), *(j--) );
-	}
-	std::swap( *j, *first );
-	return j;
+    if (last - first <= 1) {
+        return first;
+    }
+    
+    auto pivot = std::prev(last);
+    auto mid = first;
+    std::advance(mid, (last - first)/2);
+    
+    if (*pivot > *first && *pivot > *mid) {
+        if (*first > *mid)
+            pivot = first;
+        else
+            pivot = mid;
+    }
+    else if (*pivot < *first && *pivot < *mid) {
+        if (*first < *mid)
+            pivot = first;
+        else
+            pivot = mid;
+    }
+    
+    std::swap(*first, *pivot);
+    
+    auto i=std::next(first), j = std::prev(last);
+    while ( i <= j ) {
+        while(i < last && *i <= *first) ++i;
+        while(*j > *first) --j;
+        if( i < j )
+            std::swap( *(i++), *(j--) );
+    }
+    std::swap( *j, *first );
+    return j;
 }
 
 template<class RandomIt>
 void quick_sort_int(RandomIt first, RandomIt last) {
-	if (std::distance(first,last) < 42) {
-		return;
-	} else {
-		auto part = partition(first, last);
-		if (part > first) quick_sort_int(first, part);
-		if (part != last) quick_sort_int(std::next(part), last);
-	}
+    if ((last - first) < 16) {
+        insertion_sort(first, last);
+        return;
+    };
+    
+    while(first < last) {
+        auto part = partition(first, last);
+        if (part > first) quick_sort_int(first, part);
+        first = std::next(part);
+    }
 }
 
 template<class RandomIt>
 void quick_sort(RandomIt first, RandomIt last) {
-	quick_sort_int(first, last);
-	insertion_sort(first, last);
+    quick_sort_int(first, last);
 }
 
 int main() {
-	std::vector<long> a {std::istream_iterator<long>(std::cin), 
-						std::istream_iterator<long>() };
-						
-	quick_sort(a.begin(), a.end());
-	
-	unsigned long count = 1;
-	std::copy_if(a.begin(), a.end(), std::ostream_iterator<long>(std::cout, " "),
-				 [&count](long) { return !(count++ % 10); } );
-	
-	return 0;
+    std::vector<long> a {std::istream_iterator<long>(std::cin), 
+        std::istream_iterator<long>() };
+        
+        quick_sort(a.begin(), a.end());
+        
+        unsigned long count = 1;
+        std::copy_if(a.begin(), a.end(), std::ostream_iterator<long>(std::cout, " "),
+                     [&count](long) { return !(count++ % 10); } );
+        
+        return 0;
 }
