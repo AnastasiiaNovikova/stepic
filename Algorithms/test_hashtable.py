f91e7f7000755eca460481d9eddefbe1b8b134fe
Mikhail Plekhanov <m.plekhanov@netris.ru>
2016-02-01 02:44:12 +0300

    Algorithms: Hashset test script.

---
+++ Algorithms/test_hashtable.py
@@ -0,0 +1,19 @@
+#print("TEST INSERT ==================")
+for i in range(0,2000):
+    print('+ {0}'.format(i))
+
+#print("TEST FIND  ==================")
+for i in range(0,2000):
+    print('? {0}'.format(i))
+
+#print("TEST DELETE =================")
+for i in range(1000,2000):
+    print('- {0}'.format(i))
+
+#print("TEST REMAIN 1 =========")
+for i in range(0,1000):
+    print('? {0}'.format(i))
+
+#print("TEST REMAIN 2 =========")
+for i in range(1000,2000):
+    print('? {0}'.format(i))
