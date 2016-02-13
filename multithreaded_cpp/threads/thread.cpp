560f411ad56fd2e730fba67e0f7300054b35fc1f
Mikhail Plekhanov <m.plekhanov@netris.ru>
2016-02-14 02:11:31 +0300

    MC++: save pid in pthread_create example.

--- multithreaded_cpp/threads/thread.cpp
+++ multithreaded_cpp/threads/thread.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void *func(void *) {
     std::cout << "Hello from thread" << std::endl;
@@ -9,10 +16,21 @@ void *func(void *) {
     return NULL;
 }
 
-int main() {
+int main(int argc, char** argv) {
     pthread_t thread;
     void * res;
     int s;
+
+    pid_t pid = getpid();
+
+    if(argc < 2)
+        return -1;
+
+    int fd = creat(argv[1],0666);
+    char buf[50];
+    sprintf(buf, "%d", pid);
+    write(fd,buf,strlen(buf)+1);
+    close(fd);
 
     if( pthread_create(&thread, NULL, func, NULL) != 0) {
         std::cerr << "Error creating thread\n";
