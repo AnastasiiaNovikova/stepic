aec3be89d9f56c1bd90c848b676d7708c9238334
Mikhail Plekhanov <m.plekhanov@netris.ru>
2016-02-22 01:25:11 +0300

    MC++: openmp barrier example.

---
+++ multithreaded_cpp/parallel/openmp/barrier.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    #pragma omp parallel
+    {
+        std::cout << 1 << std::endl;
+        std::cout << 2 << std::endl;
+        #pragma omp barrier
+        std::cout << 3 << std::endl;
+    }
+}
